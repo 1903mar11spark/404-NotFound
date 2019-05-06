b22dbe35b80fd7c44ba649d428af6eacc86dedc2
SilviaG27 <isc.silvia.garcia@gmail.com>
2019-05-06 00:15:35 -0400

    added cards to main page

--- Angular/Four-O-Four/src/app/app.module.ts
+++ Angular/Four-O-Four/src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { ItemsComponent } from './items/items.component';
+
+import { ItemDetailComponent } from './item-detail/item-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
+    ItemsComponent,
+    ItemDetailComponent,
+    MessagesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Angular/Four-O-Four/src/app/home/home.component.ts
+++ Angular/Four-O-Four/src/app/home/home.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Item } from '../item';
+import { ItemService } from '../item.service';
+ 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: [ './home.component.css' ]
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
+  items: Item[] = [];
+ 
+  constructor(private itemService: ItemService) { }
+ 
   ngOnInit() {
+    this.getItems();
   }
-
+ 
+  getItems(): void {
+    this.itemService.getItems()
+      .subscribe(items => this.items = items.slice(0, 20));
+  }
 }
