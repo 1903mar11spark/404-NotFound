e97af690e9d58e3046dca08f4880c5054b66ab3a
HyunShin <shinreality@gmail.com>
2019-05-14 11:30:21 -0400

    derp

--- Angular/Four-O-Four/src/app/login.service.ts
+++ Angular/Four-O-Four/src/app/login.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
-@Injectable({
-  providedIn: 'root'
-})
+import { Observable, of } from 'rxjs';
+import {catchError, map, tap} from 'rxjs/operators';
+
+import { Login } from './loginI';
+import { MessageService } from './message.service';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'content-Type': 'application/json' })
+};
+
+@Injectable({providedIn: 'root'})
+
 export class LoginService {
 
-  constructor() { }
-}
+  constructor(
+    private http: HttpClient, 
+    private messageService: MessageService) { }
+
+    private urlId: string = 'http://localhost:8084/404NotFound2/login';
+
+    gettingUserId(userName: string, password: string): Observable<Login> {
+      const url = `${this.urlId}/${userName}/${password}`;
+      console.log(this.http.post(url ,""));
+      return this.http.get<Login>(url).pipe(
+        tap(_ => console.log(`fetched user id=${userName}/${password}`)),
+        catchError(this.handleError<Login>(`gettingUserId user_id=${userName}/${password}`))
+      );
+      console.log(this.http.get<Login>(url));
+    }  
+
+    private handleError<T> (operation = 'operation', result?: T) {
+      return (error: any): Observable<T> => {
+        console.error(error);
+        this.log(`${operation} failed: ${error.message}`);
+        return of(result as T);
+      };
+    }
+   
+    private log(message: string) {
+      this.messageService.add(`itemService: ${message}`);
+    }
+  }
+  
---
+++ Angular/Four-O-Four/src/app/loginI.ts
@@ -0,0 +1,6 @@
+export class Login{
+    USERNAME: string;
+    PASSWORD: string;
+    USER_ID: number;
+    USER_TYPE: number;
+}
